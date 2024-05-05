ff6b2df8504d49f3f6def1cd478d8d9da27e8a55
Milot Mirdita <milot@mirdita.de>
2024-05-06 03:26:11 +0900

    Remove debug message

--- src/lib.rs
+++ src/lib.rs
@@ -26,16 +26,6 @@ pub fn device(cpu: bool) -> Result<Device> {
     } else if metal_is_available() {
         Ok(Device::new_metal(0)?)
     } else {
-        #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
-        {
-            println!(
-                "Running on CPU, to run on GPU(metal), build this example with `--features metal`"
-            );
-        }
-        #[cfg(not(all(target_os = "macos", target_arch = "aarch64")))]
-        {
-            println!("Running on CPU, to run on GPU, build this example with `--features cuda`");
-        }
         Ok(Device::Cpu)
     }
 }
